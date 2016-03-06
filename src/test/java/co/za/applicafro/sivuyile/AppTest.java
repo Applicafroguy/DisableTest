1504857c0a3f7981391dd98d5085e31893467fd0
Sivuyile Magutywa <211208086@mycput.ac.za>
2016-03-06 02:35:18 -0800

    first commit

---
+++ src/test/java/co/za/applicafro/sivuyile/AppTest.java
@@ -0,0 +1,17 @@
+package co.za.applicafro.sivuyile;
+
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    @Ignore
+    @Test
+    public void disableTest() {
+        System.out.println("this will be ignored");
+    }
+}
